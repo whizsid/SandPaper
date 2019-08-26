161b70f858f3d834783bcc5a562501755b634034
WhizSid <whizsid@aol.com>
2019-08-27 00:12:30 +0530

    Shrink function

--- app/src/main/java/com/whizsid/sandpaper/api/Workspace.kt
+++ app/src/main/java/com/whizsid/sandpaper/api/Workspace.kt
@@ -1,5 +1,7 @@
 package com.whizsid.sandpaper.api
 
+import com.whizsid.sandpaper.api.enums.Direction
+
 class Workspace {
     private var columns:MutableList<Column> = mutableListOf()
 
@@ -15,12 +17,23 @@ class Workspace {
         return null
     }
 
+    /**
+     * Change the active column
+     *
+     * @param column Column instance to active
+     */
     fun changeColumn(column:Column):void{
         this.activeColumn = column
 
         return null
     }
 
+    /**
+     * Adding a column
+     *
+     * @param column Column to add
+     * @param index Place of the new column
+     */
     fun addColumn(column:Column,index:Int = 0):void{
         this.columns.add(index,column)
 
@@ -45,11 +58,18 @@ class Workspace {
         return null
     }
 
-    fun removeColumn(column:Column):void{
+    /**
+     * Removing a column
+     *
+     * @param column
+     *
+     * @return Weather that column is removed or not
+     */
+    fun removeColumn(column:Column):Boolean{
 
         // Not removing if one and only column
         if(this.columns.size==1)
-            return null
+            return false
 
         // Removing the column
         this.columns.removeAt(column.index)
@@ -66,11 +86,76 @@ class Workspace {
         // Indexing again
         this.index()
 
-        return null
+        return true
     }
 
-    fun shrinkLeft(column:Column,value:Int):void{
+    /**
+     * Shrink a column
+     *
+     * This function will shrink one column and adding free space to sibling column
+     *
+     * @param column Column to shrink
+     * @param value amount of shrinking
+     * @param direction Direction that shrinking
+     *
+     * @return Originally shrank amount
+     */
+    fun shrink(column:Column,value:Int,direction: Direction):Int{
+        var shrink: Int
 
+        if(direction == Direction.RIGHT){
+
+            if(column.index==this.columns.size-1){
+                return 0
+            }
+
+            val rightColumn = this.columns[column.index+1]
+
+            if(column.width-value<=1){
+
+                shrink = column.width -1
+
+                column.width = 1
+
+                rightColumn.width += shrink
+
+            } else {
+
+                rightColumn.width += value
+                column.width -= value
+
+                shrink = value
+            }
+
+            return shrink
+
+        } else if( direction == Direction.LEFT) {
+
+            if(column.index==0){
+                return 0
+            }
+
+            val leftColumn = this.columns[column.index-1]
+
+            if(leftColumn.width-value<=1) {
+                shrink = leftColumn.width - 1
+
+                leftColumn.width = 1
+                column.width += shrink
+
+            } else {
+
+                leftColumn.width -= value
+                column.width += value
+
+                shrink = value
+            }
+
+            return shrink
+
+        }
+
+        return 0
     }
 
 }
---
+++ app/src/main/java/com/whizsid/sandpaper/api/enums/Direction.kt
@@ -0,0 +1,5 @@
+package com.whizsid.sandpaper.api.enums
+
+enum class Direction {
+    LEFT,RIGHT,TOP,BOTTOM
+}
